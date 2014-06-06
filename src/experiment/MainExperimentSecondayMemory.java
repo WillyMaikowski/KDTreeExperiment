26d84439e6b7a60877546452e545e3a661656c66
mauricio <mauricio@maur-PC>
2014-06-05 23:28:41 -0400

    Experiment secondary memory

---
+++ src/experiment/MainExperimentSecondayMemory.java
@@ -0,0 +1,122 @@
+package experiment;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import KDTree.KDTree;
+import KDTree.MeanKDTreeNode;
+import KDTree.MedianKDTreeNode;
+import KDTree.Point;
+
+public class MainExperimentSecondayMemory {
+
+	public static void main( String[] args ) {
+		int minN = 25;
+		int maxN = 30;
+		KDTree experimentTree;
+		long startTime;
+		long stopTime;
+		long elapsedTime;
+		String treeName;
+		String pointName;
+		PrintWriter writer;
+		double c = 1.0;
+
+		try {
+			
+			// MeanKDTree con puntos de baja discrepancia
+			{
+				treeName = "MeanKDTree";
+				pointName = "LowDiscrepancy";
+				writer = new PrintWriter( "resultados_construccion_" + treeName
+				      + "_" + pointName + " _" + Math.random() + ".txt" );
+
+				for( int size = minN; size <= maxN; size++ ) {
+					StructExperimentContainer expCont = new StructExperimentContainer(
+					      treeName, pointName, (int) Math.pow( 2, size ) );
+
+					while( !expCont.finished() ) {
+						List<Point> listofPoints = generateLowDiscrepancyPoints( c,
+						      size );
+						startTime = System.currentTimeMillis();
+						experimentTree = new MeanKDTreeNode( listofPoints );
+						stopTime = System.currentTimeMillis();
+						elapsedTime = stopTime - startTime;
+						expCont.addObservation( experimentTree.height(), elapsedTime,
+						      sizeof( experimentTree ) );
+					}
+					writer.println( expCont.getResult() );
+					System.out.println( "Termino 2^" + size + " " + treeName + " "
+					      + pointName );
+				}
+				writer.close();
+			}
+
+		}
+		catch( FileNotFoundException e ) {
+			e.printStackTrace();
+		}
+		catch( IOException e ) {
+			e.printStackTrace();
+		}
+	}
+
+	public static int sizeof( Object obj ) throws IOException {
+
+		ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+		      byteOutputStream );
+
+		objectOutputStream.writeObject( obj );
+		objectOutputStream.flush();
+		objectOutputStream.close();
+
+		return byteOutputStream.toByteArray().length;
+	}
+
+	/**
+	 * genera 2^n puntos al azar
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public static List<Point> generateRandomPoints( double c, int n ) {
+		List<Point> result = new ArrayList<Point>();
+		double numPoints = Math.pow( 2, n );
+		for( int i = 1; i <= numPoints; i++ ) {
+			result.add( generateRandomPoint( 0, 0, ( c * Math.sqrt( numPoints ) ),
+			      ( c * Math.sqrt( numPoints ) ) ) );
+		}
+		return result;
+	}
+
+	public static List<Point> generateLowDiscrepancyPoints( double c, int n ) {
+		List<Point> result = new ArrayList<Point>();
+		double numPoints = Math.pow( 2, n );
+		double delta = c;
+
+		for( double i = 0; i < c * Math.sqrt( numPoints ); i = i + delta ) {
+			for( double j = 0; j < c * Math.sqrt( numPoints ); j = j + delta ) {
+				result.add( generateRandomPoint( i, j, i + delta, j + delta ) );
+			}
+		}
+
+		return result;
+	}
+
+	public static Point generateRandomPoint( double x0, double y0, double x1,
+	      double y1 ) {
+		Point result = new Point( random( x0, x1 ), random( y0, y1 ) );
+		return result;
+	}
+
+	public static double random( double i, double n ) {
+		return i + ( n - i ) * Math.random();
+	}
+
+}
---
+++ src/experiment/SecondaryMemoryExperimentContainer.java
@@ -0,0 +1,48 @@
+package experiment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondaryMemoryExperimentContainer extends StructExperimentContainer{
+
+	
+	private List<Integer> accessContainer;
+	private double accessSum;
+	
+	public SecondaryMemoryExperimentContainer( String kdTreeName,
+         String pointType, int size ) {
+	   super( kdTreeName, pointType, size );
+	   this.accessContainer = new ArrayList<Integer>();
+	   accessSum=0;
+   }
+	
+	
+	public void addObservation( int height, long time, int size, int numAccess ) {
+		super.addObservation( height, time, size );
+		this.accessContainer.add( numAccess );
+		this.accessSum += numAccess;
+	}
+	
+	public double getAverageAccess() {
+		if( this.getNumberOBservations() > 0 ) return 1.0 * this.accessSum / this.getNumberOBservations();
+		return 0.0;
+	} 
+	
+	public double getAccessStd() {
+		if( this.getNumberOBservations() == 0 ) return 0;
+		double avg = this.getAverageAccess();
+		double var = 0;
+
+		for( int access : this.accessContainer ) {
+			var += Math.pow( access - avg, 2 );
+		}
+		return Math.sqrt( var / this.getNumberOBservations() );
+	}
+	
+	public String toString(){
+		return super.toString()+ "\t" + this.getAverageAccess()+"\t"+this.getAccessStd();
+	}
+	
+	
+
+}
